d8895f105cac6f9a87ddb53f08567d1a4fcffd4e
katherin_moreno <morenodavilakatherin@gmail.com>
2020-07-16 16:29:02 -0500

    alert javaScript

--- src/index.js
+++ src/index.js
@@ -3,3 +3,20 @@ import validator from './validator.js';
 console.log(validator);
 
 
+alert("ES IMPORTANTE VALIDAR TU TARJETA PARA CONTINUAR CON TU COMPRA");
+
+
+
+
+
+
+function guardarUsuario()
+{
+    nombre = document.getElementById('inputNombre').value 
+    document.getElementById('nombre').textContent = nombre
+    document.getElementById("inicio").style.display='none'
+    document.getElementById("bienvenido").style.display='block'
+    document.getElementById("seleccion").style.display='block'
+
+  }
+
